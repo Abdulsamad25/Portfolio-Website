96ab634f7683ce5cae2d883ec7221a73320a3196
Abdulsamad25 <samadolalekan15@gmail.com>
2025-02-01 19:36:06 +0100

    new change

--- portfolio/src/js/scripts.js
+++ portfolio/src/js/scripts.js
@@ -1,1 +1,30 @@
-// This file is intentionally left blank.
+function toggleMenu() {
+  const navLinks = document.getElementById('navLinks');
+  navLinks.classList.toggle('show');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const links = document.querySelectorAll('.nav-links .links');
+
+  links.forEach(link => {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+      links.forEach(link => link.classList.remove('active'));
+      this.classList.add('active');
+
+      const targetId = this.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: 'smooth'
+        });
+      }
+
+      // Close the menu after clicking a link in mobile view
+      const navLinks = document.getElementById('navLinks');
+      if (navLinks.classList.contains('show')) {
+        navLinks.classList.remove('show');
+      }
+    });
+  });
+});
